0e980586fe607e2481e22ff7644776eda6056ec8
Tomas Vestenicky <vestenicky@icloud.com>
2019-11-24 11:39:26 +0100

    Set up Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,58 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    title: `Lunadio`,
+    name: `Lunadio`,
+    siteUrl: `https://lunadio.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
       heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
-      maxWidth: 652,
+      maxWidth: 652
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/narative`
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/narative`
       },
       {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://instagram.com/narative.co`
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
+        url: `https://www.linkedin.com/company/narative/`
       },
       {
         name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
-    ],
+        url: `https://dribbble.com/narativestudio`
+      }
+    ]
   },
   plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     {
       resolve: "@narative/gatsby-theme-novela",
       options: {
         contentPosts: "content/posts",
         contentAuthors: "content/authors",
         basePath: "/",
-        authorsPage: true,
+        authorsPage: false,
         sources: {
-          local: true,
-          // contentful: true,
-        },
-      },
+          local: false,
+          contentful: true
+        }
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -54,8 +63,8 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `standalone`,
-        icon: `src/assets/favicon.png`,
-      },
-    },
-  ],
+        icon: `src/assets/favicon.png`
+      }
+    }
+  ]
 };
